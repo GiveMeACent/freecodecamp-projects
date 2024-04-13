f3f413eacfc946cfc4e2bc6f248e073a79e8edbb
GiveMeACent <anto.coltry@gmail.com>
2024-04-13 17:01:15 +0200

    Completed Random Quote Machine

--- front-end-development/random-quote-machine/src/QuoteMachine.jsx
+++ front-end-development/random-quote-machine/src/QuoteMachine.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import "./QuoteMachine.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTumblr, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 const QuoteMachine = (props) => {
   const [quotes, setQuotes] = useState(null);
   const [quote, setQuote] = useState("");
+  const [currentColor, setCurrentColor] = useState("");
+  const colors = [
+    "#ff0e19",
+    "#ff9000",
+    "#ffcb00",
+    "#00de51",
+    "#19c9ff",
+    "#0078ff",
+    "#5b51de",
+    "#ff004f",
+  ];
 
   useEffect(() => {
+    setCurrentColor(generateRandomColor());
     (async () => {
       const tmp = (
         await Axios.get(
@@ -22,6 +37,11 @@ const QuoteMachine = (props) => {
     return quotes[randomIndexQuote];
   };
 
+  const generateRandomColor = () => {
+    const randomIndexColor = Math.round(Math.random() * (colors.length - 1));
+    return colors[randomIndexColor];
+  };
+
   const handleClickGenerateQuote = () => {
     if (quotes) setQuote(generateRandomQuote());
   };
@@ -30,13 +50,21 @@ const QuoteMachine = (props) => {
     handleClickGenerateQuote();
   }, [quotes]);
 
+  useEffect(() => {
+    console.log(document.body);
+    document.body.style.background = currentColor;
+  }, [currentColor]);
+
   return (
     <>
-      <div id="quote-box">
+      <div id="quote-box" style={{ "--current-color": currentColor }}>
         {quotes ? (
           <>
-            <h1 id="text">{quote.quote}</h1>
-            <h1 id="author">{quote.author}</h1>
+            <h1 id="text">
+              <FontAwesomeIcon className="quotes" icon={faQuoteLeft} />
+              {quote.quote}
+            </h1>
+            <p id="author">- {quote.author}</p>
             <div className="wrapper">
               <div className="socials">
                 <a
@@ -44,18 +72,23 @@ const QuoteMachine = (props) => {
                   target="_blank"
                   id="tweet-quote"
                   className="social-button"
-                ></a>
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
                 <a
                   href="https://www.tumblr.com/"
                   target="_blank"
                   id="tumblr-quote"
                   className="social-button"
-                ></a>
+                >
+                  <FontAwesomeIcon icon={faTumblr} />
+                </a>
               </div>
               <button
                 id="new-quote"
                 onClick={() => {
                   handleClickGenerateQuote();
+                  setCurrentColor(generateRandomColor());
                 }}
               >
                 New quote
