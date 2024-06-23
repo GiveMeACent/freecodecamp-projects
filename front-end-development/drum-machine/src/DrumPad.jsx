42375ebbf525f43de476ac059c17084068a36c01
GiveMeACent <anto.coltry@gmail.com>
2024-06-23 16:56:14 +0200

    fix audio paused

--- front-end-development/drum-machine/src/DrumPad.jsx
+++ front-end-development/drum-machine/src/DrumPad.jsx
@@ -3,18 +3,15 @@ import "./DrumPad.css";
 
 const DrumPad = (props) => {
   const [audio, setAudio] = useState(null);
-  const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
     setAudio(new Audio(props.url));
   }, [props.url]);
 
   const handleClickPlayAudio = () => {
-    if (playing) audio.currentTime = 0;
+    if (!audio.paused) audio.currentTime = 0;
 
     audio.play();
-
-    setPlaying(!playing);
   };
   return (
     <button
